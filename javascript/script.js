437bcf358fb536122a45d3ca33213e8b5327f01f
Sofia Dias <106925364+sofiadias87@users.noreply.github.com>
2023-07-21 18:15:21 +0100

    menu adufe

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,26 @@
 window.addEventListener('DOMContentLoaded', function() {
-    var navbar = document.querySelector('.navbar');
-    var burger = document.querySelector('.burger');
-    var navLinks = document.querySelector('.nav-links');
-  
-    burger.addEventListener('click', function() {
-      navLinks.classList.toggle('active');
-    });
-  
-    window.addEventListener('scroll', function() {
-      if (window.scrollY > 0) {
-        navbar.classList.add('scrolled');
-      } else {
-        navbar.classList.remove('scrolled');
-      }
-    });
+  var navbar = document.querySelector('.navbar');
+  var burger = document.querySelector('.burger');
+  var navLinks = document.querySelector('.nav-links');
+
+  function closeMenu() {
+    navLinks.classList.remove('active');
+  }
+
+  burger.addEventListener('click', function() {
+    navLinks.classList.toggle('active');
+  });
+
+  var menuItems = document.querySelectorAll('.nav-links li a');
+  for (var i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', closeMenu);
+  }
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
 });
