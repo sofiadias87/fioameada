44e4b3db4a3126efa189d2c0a3dfa54d65f1ee52
Sofia Dias <106925364+sofiadias87@users.noreply.github.com>
2023-07-25 19:55:03 +0100

    fix bugs

--- javascript/reveal.js
+++ javascript/reveal.js
@@ -1,4 +1,4 @@
-window.sr = ScrollReveal({ reset: true });
-sr.reveal(".title-banner1", { duration: 1000 });
-sr.reveal(".btn-contact", { delay: 500 });
-sr.reveal(".scroll", { delay: 700 });
+// window.sr = ScrollReveal({ reset: true });
+// sr.reveal(".title-banner1", { duration: 1000 });
+// sr.reveal(".btn-contact", { delay: 500 });
+// sr.reveal(".scroll", { delay: 700 });
