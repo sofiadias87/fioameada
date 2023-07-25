62662fe1e5eb45794fa80cda5cc4d3c715adfa40
Sofia Dias <106925364+sofiadias87@users.noreply.github.com>
2023-07-25 19:58:10 +0100

    Delete reveal.js

--- javascript/reveal.js
+++
@@ -1,4 +0,0 @@
-// window.sr = ScrollReveal({ reset: true });
-// sr.reveal(".title-banner1", { duration: 1000 });
-// sr.reveal(".btn-contact", { delay: 500 });
-// sr.reveal(".scroll", { delay: 700 });
